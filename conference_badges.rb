6dc56d092f8308b434863973d7e76b240772779a
Eric Farber <eric.j.farber@gmail.com>
2017-03-25 22:00:33 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,29 @@
 def badge_maker(name)
     return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+    badges = []
+    array.each do |name|
+         badges.push("Hello, my name is #{name}.")
+    end
+ badges
+end
+
+def assign_rooms(array)
+    assignments = []
+    array.each_with_index do |name, index|
+        assignments.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+    end
+    assignments
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |x|
+        puts x
+    end
+    assign_rooms(array).each do |y|
+        puts y
+end
+
+end
